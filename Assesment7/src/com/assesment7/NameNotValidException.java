dab56050ec43efc633017fa7cef29fc2d4a46389
siva1234s <kotasivareddy30@gmail.com>
2022-04-29 16:56:36 +0530

    Add Assessments

---
+++ Assesment7/src/com/assesment7/NameNotValidException.java
@@ -0,0 +1,10 @@
+package com.assesment7;
+
+
+public class NameNotValidException extends Exception
+{
+    public String validname()
+    {
+         return ("Name is not Valid..Please ReEnter the Name");
+    }
+}
